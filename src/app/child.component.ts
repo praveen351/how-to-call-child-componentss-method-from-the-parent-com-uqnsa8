331b0264a5db4ece79bc51171fe78ce7d4e13b68
praveen351 <praveenkumar1234raja@gmail.com>
2020-05-27 16:06:13 +0530

    1 file modified

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'child',
-  template: `<h3>{{test}}</h3>`,
+  template: `<h3>Hiii {{test}}</h3>`,
 })
 export class ChildComponent {
   test: String;
